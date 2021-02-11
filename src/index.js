0aaed0b2a5e7dc08e5aa36b7a7b7095c9a76979b
Shakhrom Mukumov <leon9208@mail.ru>
2021-02-11 07:40:57 +0300

    Получение данных по API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
+import 'regenerator-runtime/runtime';
 import './main.sass';
+import axios from 'axios';
+
+const API_KEY = 'a5837db97d72016c81a7a776f4240db9'
+const url = 'https://gateway.marvel.com/v1/public/comics';
+
+class GetDataApi {
+  async getData(url) {
+    const response = await axios.get(url, {
+      params: {
+        apikey: API_KEY
+      }
+    });
+
+    console.log(response.data.data.result);
+  }
+}
+
+const getDataApi = new GetDataApi();
+
+getDataApi.getData(url)
